03fb2df770860ca195c13e31e93514bebad789cc
Jonathan Chiu <jchiu09@gmail.com>
2023-01-22 17:52:24 -0800

    Update DiskProviderBase.cs

--- src/NzbDrone.Common/Disk/DiskProviderBase.cs
+++ src/NzbDrone.Common/Disk/DiskProviderBase.cs
@@ -217,6 +217,33 @@ namespace NzbDrone.Common.Disk
             CopyFileInternal(source, destination, overwrite);
         }
 
+        private void CopyFileWithTempFile(string source, string destination, bool overwrite = false)
+        {
+            TransferFileWithTempFile(source, destination, overwrite, false);
+        }
+
+        private void MoveFileWithTempFile(string source, string destination)
+        {
+            TransferFileWithTempFile(source, destination, true, true);
+        }
+
+        private void TransferFileWithTempFile(string source, string destination, bool overwrite = false, bool deleteSource = false)
+        {
+            var tempDestination = destination + ".transfer~";
+
+            // Copy to temporary file
+            File.Copy(source, tempDestination, true);
+
+            // Move to final file name once temporary file is done copying
+            File.Move(tempDestination, destination);
+
+            // Delete the source file if transfer was a move action
+            if (deleteSource)
+            {
+                File.Delete(source);
+            }
+        }
+
         public void CopyFile(string source, string destination, bool overwrite = false)
         {
             Ensure.That(source, () => source).IsValidPath();
@@ -232,7 +259,7 @@ namespace NzbDrone.Common.Disk
 
         protected virtual void CopyFileInternal(string source, string destination, bool overwrite = false)
         {
-            File.Copy(source, destination, overwrite);
+            CopyFileWithTempFile(source, destination, overwrite);
         }
 
         public void MoveFile(string source, string destination, bool overwrite = false)
@@ -269,7 +296,7 @@ namespace NzbDrone.Common.Disk
                 throw new FileAlreadyExistsException("File already exists", destination);
             }
 
-            File.Move(source, destination);
+            MoveFileWithTempFile(source, destination);
         }
 
         public virtual bool TryRenameFile(string source, string destination)
